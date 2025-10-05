0ad458a2fd15e378d242f483e171d969ae705bda
lucassilvalopes <47322712+lucassilvalopes@users.noreply.github.com>
2025-10-05 09:47:42 -0300

    visuals

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,60 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+        <q-input
+          v-model="text"
+          filled
+          type="textarea"
+          rows="19"
+          dark
+        >
+        </q-input>
+    </div>
+    <div class="col text-white align-middle text-right q-pt-md q-px-md">
+      <q-btn 
+        @click="copy"
+        outline rounded color="white"
+      >
+        <span class="material-icons">
+          content_copy
+        </span>
+        &nbsp;
+        Copy
+      </q-btn>
+    </div>
+    <div class="col text-center q-pt-lg q-px-md">
+      <q-input
+        v-model="myText"
+        type="textarea"
+        rows="19"
+        filled
+        readonly
+        dark
+      />
+    </div>
   </q-page>
+
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      text: '',
+      myText: `This is a multiline text.
+      The user can see this, but not edit it.`
+    }
+  },
+  methods: {
+    copy(){
+      console.log("pressed the copy button")
+    }
+  }
 }
 </script>
+
+<style lang="sass">
+  .q-page
+    background: linear-gradient(to bottom, #136a8a, #267871)
+</style>
